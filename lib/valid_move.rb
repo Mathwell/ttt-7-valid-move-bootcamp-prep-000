d4791c37b85294cf0ab19b263bead85a42decc61
Olena Ageyeva <eageeva@hotmail.com>
2017-09-24 18:51:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -17,11 +17,6 @@ end
 def position_taken?(board, number)
   index=number.to_i
   if valid_move?(board,index)
-
-   if board[index]==" " || board[index]==""
-     return false
-   elsif  board[index]=="X" || board[index]=="O"
-       return true
-   end
+     return false 
   end
 end
