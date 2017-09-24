041befa8511117c6d277fa129220536c6d46b2de
Olena Ageyeva <eageeva@hotmail.com>
2017-09-24 18:50:45 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,11 @@ def valid_move?(board, index)
    if board.length==0 || index<0 || board[index]==nil
      return false
    elsif (index>=0 && index<9)
-     return true
+     if  board[index]=="X" || board[index]=="O"
+       return false
+     elsif board[index]==" " || board[index]==""
+        return true
+     else return false
    else
      return false
    end
